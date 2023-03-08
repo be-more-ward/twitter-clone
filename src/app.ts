d26a72ec8404d44798314299bc34f8cc20b8c836
be-more-ward <119626790+be-more-ward@users.noreply.github.com>
2023-03-08 18:20:04 -0300

    Add basic error handler and custom error

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import "dotenv/config"
+import "express-async-errors"
 import express from "express"
 const app = express()
 
@@ -8,11 +9,16 @@ app.use(express.json())
 //Routers
 import authRouter from "./routes/authRoutes"
 
+//Middlewares
+import { notFound } from "./middleware/not-found"
+
 app.get("/",(req,res)=>{
     res.send("test")
 })
 
 app.use("/api/v1/auth", authRouter)
+
+app.use(notFound)
 
 
 const port = process.env.PORT || 3000
