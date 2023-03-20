78cbff4165411d63712fc294c8df1dd1082edac6
be-more-ward <119626790+be-more-ward@users.noreply.github.com>
2023-03-20 16:47:04 -0300

    Fix: Add regex pattern to register and login schemas

--- src/validate/schemas.ts
+++ src/validate/schemas.ts
@@ -1,13 +1,13 @@
 import Joi from "joi"
 
 export const registerSchema = Joi.object({
-    username: Joi.string().min(4).max(30).required(),
+    username: Joi.string().pattern(/^[a-zA-Z0-9_]+$/).min(4).max(30).required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(5).max(50).required()
 })
 
 export const loginSchema = Joi.object({
-    username: Joi.string().min(4).max(20).optional(),
+    username: Joi.string().pattern(/^[a-zA-Z0-9_]+$/).min(4).max(20).optional(),
     email: Joi.string().email().optional(),
     password: Joi.string().min(5).max(50).required()
 })
