c80d979390a0e9d20a5926d0c612325d652602fb
be-more-ward <119626790+be-more-ward@users.noreply.github.com>
2023-04-15 17:59:04 -0300

    Update auth.test for login tests

--- test/auth.test.ts
+++ test/auth.test.ts
@@ -2,10 +2,23 @@ import { PrismaClient } from '@prisma/client'
 import request from "supertest"
 import app from "../src/app"
 
+import { hashPassword } from '../src/utils/bcrypt'
+
 const prisma = new PrismaClient()
 
 beforeAll( async ()=>{
     await prisma.$connect()
+
+    //new user for test in login
+    const user =await prisma.user.create({
+        data:{
+            username: "john",
+            email: "john@gmail.com",
+            password: await hashPassword("password")
+        }
+    })
+    
+    console.log("new user created");
 })
 
 afterAll(async()=>{
@@ -71,4 +84,83 @@ describe("Register tests", ()=>{
         expect(response.body.msg).toEqual(`\"username\" length must be less than or equal to 30 characters long`)
     })
     
+})
+
+describe("Login tests", ()=>{
+    test("should return 200 for login with username only (valid data)", async ()=>{
+        const response = await request(app).post("/api/v1/auth/login").send(
+            {
+                username: "anna",
+                password: "password"
+            }
+        )
+
+        expect(response.status).toBe(200)
+    })
+
+    test("should return 200 for login with email only (valid data)", async ()=>{
+        const response = await request(app).post("/api/v1/auth/login").send(
+            {
+                email: "anna@gmail.com",
+                password: "password"
+            }
+        )
+
+        expect(response.status).toBe(200)
+    })
+
+    test("should return 200 for login with user created before tests", async ()=>{
+        const response = await request(app).post("/api/v1/auth/login").send(
+            {
+                email: "john@gmail.com",
+                password: "password"
+            }
+        )
+        
+        expect(response.status).toBe(200)
+    })
+
+    test("should return 401 for login with invalid credentials",async () => {
+        const response = await request(app).post("/api/v1/auth/login").send(
+            {
+                username: "john",
+                password: "pasworddd"
+            }
+        )
+        expect(response.status).toBe(401)
+        expect(response.body.msg).toBe("credentials are invalid")
+    })
+
+    test("should return 404 for login with non-existent user",async () => {
+        const response = await request(app).post("/api/v1/auth/login").send(
+            {
+                username: "mike",
+                password: "mikePassword"
+            }
+        )
+        expect(response.status).toBe(404)
+        expect(response.body.msg).toBe("credentials are invalid")
+    })
+
+    test("should return 400 for login with missing data (no username or email)", async ()=>{
+        const response = await request(app).post("/api/v1/auth/login").send(
+            {
+                password:"passwordd"
+            }
+        )
+
+        expect(response.status).toBe(400)
+        expect(response.body.msg).toBe("Please provide username or email")
+    })
+
+    test("should return 400 for login with missing data (no password)", async ()=>{
+        const response = await request(app).post("/api/v1/auth/login").send(
+            {
+                email:"john@gmail.com"
+            }
+        )
+
+        expect(response.status).toBe(400)
+        expect(response.body.msg).toBe(`\"password\" is required`)
+    })
 })
